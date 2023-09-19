c4b29de4887dd3ce1dc00a5bde3b9a7085018e20
Michael Giglia <michael.a.giglia@gmail.com>
2023-09-19 15:41:17 -0400

    add button state machine example

---
+++ state-machines/sm1.cpp
@@ -0,0 +1,57 @@
+#include <rcc_stdlib.h>
+using namespace std;
+
+int main(void)
+{
+    stdio_init_all();
+    sleep_ms(1500);
+    cyw43_arch_init();
+    cyw43_arch_gpio_put(0,1); //turns on LED
+    rcc_init_pushbutton();
+
+//State Transition diagram variables
+    int state=1; //the 'state' we are currently in
+    double duration = 1.0; //the button press duration we're looking for
+    unsigned long start_time; //the time the button was first pressed
+    unsigned long current_time;//the current time
+
+    while(true){
+    //State 1
+        if(state == 1)
+        {
+            if(!gpio_get(RCC_PUSHBUTTON))
+            {
+            //Button has been pressed, transition to state 2,
+            //set start time variable to current time
+            state = 2;
+            start_time = time_us_32();
+            }
+        }
+        
+    //State 2
+        if(state == 2)
+        {
+            //Check if button is released, if so go back to state 1
+            // and print "long press NOT detected"
+            if(gpio_get(RCC_PUSHBUTTON)) { state = 1; cout << "long press NOT detected\n";}
+            //Check if button is still pressed, if so check duration
+            if(!gpio_get(RCC_PUSHBUTTON))
+            {
+            //Get current time
+            current_time = time_us_32();
+            if((current_time - start_time) >= duration*1000000.0)
+            {
+                //Button has been held for 1 second, move to state 3
+                state = 3;
+            }
+            }
+        }
+    //State 3
+        if(state == 3)
+        {
+            //reset back to state 1 and print "long button press detected"
+            state = 1;
+            cout << "long button press detected\n";
+        }
+    }
+}
